b1ec8a57eede21a6a3e7f3246eda2a26a93124f4
Dom.Hofer <dmand6666@gmail.com>
2020-09-08 18:29:13 +0200

    Fixed routing

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="http://localhost:8080/#/">Current Weather</a>
+      <router-link to="/" class="navbar-brand">Current Weather</router-link>
       <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
         <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle"
         data-toggle="dropdown" href="#" role="button">Predictions</a>
         <div class="dropdown-menu">
-          <a class="dropdown-item" href="http://localhost:8080/#/hourly">Hourly Predictions</a>
+          <router-link to="/hourly" class="dropdown-item" >Hourly Predictions</router-link>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="http://localhost:8080/#/daily">Daily Predictions</a>
+          <router-link to="/daily" class="dropdown-item">Daily Predictions</router-link>
         </div>
       </li>
         </ul>
